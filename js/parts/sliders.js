0d7fc2a36a65c28a19c7f3107a4ae5359ea46eab
Yuriy Savchenko <yurkaronin@gmail.com>
2023-06-14 20:43:11 +0300

    Обновил файлы проекта

--- js/parts/sliders.js
+++ js/parts/sliders.js
@@ -1,46 +1,32 @@
 // Этот код создаёт слайдер с использованием библиотеки Swiper на элементе с классом .first-screen__slider, если такой элемент существует на странице.
 
-// Проверяем, существует ли элемент с классом '.first-screen__slider' на странице.
 if (document.querySelector(".first-screen__slider")) {
-  // Создаём новый экземпляр Swiper для слайдера с указанными настройками.
   var swiper = new Swiper(".first-screen__slider", {
-    // Конфигурация пагинации слайдера.
     pagination: {
-      // Указываем элемент, который будет использоваться для пагинации.
       el: ".first-screen__slider .swiper-pagination",
-      // Делаем пагинацию кликабельной.
       clickable: true,
     },
   });
 }
 
-// В этом скрипте используется библиотека Swiper, которая является мощным инструментом для создания слайдеров и каруселей.
-// Элемент с классом .first-screen__slider становится контейнером слайдера.
-// Внутри него ожидается структура, соответствующая требованиям Swiper.
-// Пагинация (индикаторы текущего слайда) будет располагаться внутри контейнера слайдера, в элементе с классом .swiper-pagination.
-// Этот элемент становится кликабельным, что позволяет пользователю переходить к определенному слайду, нажимая на соответствующий индикатор пагинации.
 
 if (document.querySelector(".partners__slider")) {
   var swiper = new Swiper(".partners__slider", {
-    // slidesPerView: 4,
-    // spaceBetween: 32,
     grabCursor: true,
+    loop: true,
     pagination: {
       el: ".partners__slider .swiper-pagination",
       clickable: true,
     },
     breakpoints: {
-      // when window width is >= 320px
       320: {
         slidesPerView: 2,
         spaceBetween: 16,
       },
-      // when window width is >= 480px
       550: {
         slidesPerView: 3,
         spaceBetween: 16,
       },
-      // when window width is >= 640px
       1024: {
         slidesPerView: 4,
         spaceBetween: 32,
@@ -49,10 +35,33 @@ if (document.querySelector(".partners__slider")) {
   });
 }
 
+if (document.querySelector(".delivery")) {
+  var swiper = new Swiper(".delivery .swiper", {
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".delivery .swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 3,
+        spaceBetween: 16,
+      },
+      550: {
+        slidesPerView: 4,
+        spaceBetween: 16,
+      },
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 32,
+      },
+    },
+  });
+}
+
 if (document.querySelector(".licenses-slider .swiper")) {
   var swiper = new Swiper(".licenses-slider .swiper", {
-    // slidesPerView: 4,
-    // spaceBetween: 32,
     grabCursor: true,
     loop: true,
     pagination: {
@@ -60,7 +69,6 @@ if (document.querySelector(".licenses-slider .swiper")) {
       clickable: true,
     },
     breakpoints: {
-      // when window width is >= 320px
       320: {
         slidesPerView: 1.5,
         spaceBetween: 16,
@@ -69,12 +77,10 @@ if (document.querySelector(".licenses-slider .swiper")) {
         slidesPerView: 2,
         spaceBetween: 16,
       },
-      // when window width is >= 480px
       768: {
         slidesPerView: 3,
         spaceBetween: 16,
       },
-      // when window width is >= 640px
       1200: {
         slidesPerView: 4,
         spaceBetween: 32,
